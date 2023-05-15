f6a5f4b4feb2e7ac0dad81071d9d184f93ac881a
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-15 09:15:20 +0400

    adding treesitter context

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,6 +4,8 @@ return { 'nvim-lua/popup.nvim', 'sudormrfbin/cheatsheet.nvim',
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
   },
+   "nvim-treesitter/nvim-treesitter-context"
+  ,
   {
     'AlexvZyl/nordic.nvim',
     lazy = false,
