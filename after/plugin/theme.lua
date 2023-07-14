ab3224ff6ccdf0fb3e0c6437cb0e421a71b80b05
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-07-14 18:18:30 +0400

    adding system clipboard remaps

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -2,8 +2,8 @@ function SetColors(theme)
 	theme = theme or "github_dark"
 	vim.cmd.colorscheme(theme)
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "#131313" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 SetColors()
